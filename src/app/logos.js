e6ae8db0c2ce1d945813db4015aceff1538638a4
tomherni <tomherni@gmail.com>
2020-12-09 20:39:48 +0100

    update logo paths

--- src/app/logos.js
+++ src/app/logos.js
@@ -2,45 +2,45 @@ export const logos = {
   2018: [
     {
       title: 'frontx',
-      path: '2018/frontx.png',
+      path: '2018-frontx.png',
       text: ['The frontx original logo in all its glory'],
     },
     {
       title: 'frontx Canada',
-      path: '2018/canada.png',
+      path: '2018-canada.png',
       text: ['An epic journey to Canada, eh', 'Only the strongest returned'],
     },
     {
       title: 'frontx Christmas',
-      path: '2018/christmas.png',
+      path: '2018-christmas.png',
     },
   ],
   2019: [
     {
       title: 'frontx 2019',
-      path: '2019/2019.png',
+      path: '2019-new-years.png',
     },
     {
       title: 'frontx skiing trip',
-      path: '2019/skiing.png',
+      path: '2019-skiing.png',
       text: ['Fun was had', 'And bones were broken'],
     },
     {
       title: 'frontx Bob',
-      path: '2019/bob.png',
+      path: '2019-bob.png',
     },
     {
       title: 'frontx New York',
-      path: '2019/new-york.png',
+      path: '2019-new-york.png',
     },
     {
       title: 'frontx New York (deluxe)',
-      path: '2019/new-york-on-steroids.jpg',
+      path: '2019-new-york-on-steroids.jpg',
       text: ['Same New York, but on steroids'],
     },
     {
       title: 'frontx Prague',
-      path: '2019/prague.png',
+      path: '2019-prague.png',
       text: [
         'Great conference, great city',
         'Beer cheaper than water also helped',
@@ -48,35 +48,35 @@ export const logos = {
     },
     {
       title: 'frontx partner weekend',
-      path: '2019/partner-weekend.png',
+      path: '2019-partner-weekend.png',
       text: ['Nerds and their partners teaming up'],
     },
     {
       title: 'frontx FrontMania',
-      path: '2019/frontmania.png',
+      path: '2019-frontmania.png',
     },
     {
       title: 'frontx Christmas',
-      path: '2019/christmas.png',
+      path: '2019-christmas.png',
     },
   ],
   2020: [
     {
       title: 'frontx 2020',
-      path: '2020/2020.png',
+      path: '2020-new-years.png',
     },
     {
       title: 'frontx skiing trip',
-      path: '2020/skiing.png',
+      path: '2020-skiing.png',
     },
     {
       title: 'frontx social distancing',
-      path: '2020/covid-19-distancing.png',
+      path: '2020-covid-19-social-distancing.png',
       text: ['COVID-19 is not playing around'],
     },
     {
       title: 'frontx Christmas',
-      path: '2020/christmas.png',
+      path: '2020-christmas.png',
     },
   ],
 };
